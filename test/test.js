085540a6fb98427085226e6cd338c3528c7454dc
Jason Quense <monastic.panic@gmail.com>
2018-04-26 16:01:07 -0400

    fix setLevel

--- test/test.js
+++ test/test.js
@@ -3,6 +3,8 @@ const { logger, setLevel } = require('../index');
 setLevel('debug');
 
 logger.info('foo asfasfasfasf');
+logger.debug('foo');
+
 logger.add('socket', 'Socket.IO').info('foo asfasfasfasf');
 
 logger
