0eaf49ccacaa7d96e3acc2d1eaae99f01331136c
Stephen Liu <sliu@quantum-si.com>
2019-12-30 18:53:29 -0500

    Fix up 4C_LOGGING_LEVEL setting logic

--- createLogging.js
+++ createLogging.js
@@ -36,10 +36,9 @@ module.exports = function createLogging({
     : Env.get.boolish('4C_LOGGING_USE_COLOR', true);
 
   // TODO: deprecate 4C_* as number prefixed env variables aren't standard
-  let LEVEL = Env.get('4C_LOGGING_LEVEL', null);
-  if (!LEVEL) {
-    LEVEL = Env.get('FOURC_LOGGING_LEVEL', 'info');
-  }
+  const LEVEL = Env.get('4C_LOGGING_LEVEL', null) === null
+    ? Env.get('FOURC_LOGGING_LEVEL', 'info')
+    : Env.get('4C_LOGGING_LEVEL', 'info');
 
   const appendMeta = format(i => {
     const { level: _, message: _a, label: _b, ...meta } = i;
